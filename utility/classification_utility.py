69975f1eb89db78bfa844b86c31d57f390651a5c
DavideB45 <dadewww@gmail.com>
2024-12-27 13:18:59 +0100

    vado a mangiare poi mi ci rimetto

--- utility/classification_utility.py
+++ utility/classification_utility.py
@@ -69,3 +69,52 @@ def define_target(merged_fd:pd.DataFrame)-> pd.DataFrame:
     '''
     merged_fd['target'] = merged_fd['position'].apply(lambda x: x<=20)
     return merged_fd
+
+# TODO: This actually can take the dataframe
+EXPERIENCE_LEVELS = ['beginner', 'developing', 'competitive', 'semi-pro', 'pro']
+def compute_experience(row: pd.Series) -> float:
+    '''
+    Compute the experience level of a cyclist based on the given row.
+
+    args:
+        - row (pd.Series): a row of the dataframe containing cyclist data
+
+    returns:
+        - float: the computed experience level
+    '''
+    bins = [0, 15, 50, 100, 200, float('inf')]
+    # Example computation: experience level based on race count and age
+    experience = row['race_count'] * (row['cyclist_age_cyc'] / 100)
+    cyclist_df['experience_level'] = pd.cut(cyclist_df['race_count'], bins=bins, labels=experience_levels, right=False)
+    return experience
+
+def compute_total_points(merged_df: pd.DataFrame) -> pd.DataFrame:
+    '''
+    Compute the total points of each cyclist at a given moment
+
+    args:
+        - merged_df (pd.DataFrame): the dataframe on which recompute the total points
+
+    returns:
+        - pd.DataFrame: the updated dataframe
+    '''
+    # The original function used
+    # punti_totali = races_df.groupby('cyclist')['points'].sum()
+    # cyclist_df['total_points'] = cyclist_df['_url'].apply(lambda x: punti_totali[x] if x in punti_totali.index else 0)
+    
+    # OTHER STUFF THAT CAN BE COMPUTED HERE
+    # 'victories_by_points', 
+    # 'avg_points_per_race', 
+    # 'average_position', 
+    # 'avg_speed_cyclist', 
+    # 'mean_stamina_index'
+    #
+    # O(NlogN) to order by date
+    # NOTE: the 2 operations below shoul be done in one pass over the data to minimize I/O complexity
+    # O(N) scan to create an hash table with all names of cyclists 
+    # (it will be a generic hash Ferragina would be disappointed)
+        # What should be in the hash table:
+        # points reached up to now
+        # points won in the last race
+        # values to compute other metrics
+    # O(N) scan and compute the value
